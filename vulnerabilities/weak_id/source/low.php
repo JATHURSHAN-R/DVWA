3a24b712d1d7c51b53868ae165c2b7cd1fd52441
Jathurshan.R <rdjathurshan@gmail.com>
2026-08-29 01:08:41 -0400

    fix: replace predictable session ID counter with cryptographically random token

--- vulnerabilities/weak_id/source/low.php
+++ vulnerabilities/weak_id/source/low.php
@@ -1,13 +1,8 @@
 <?php
 
-$html = "";
-
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	if (!isset ($_SESSION['last_session_id'])) {
-		$_SESSION['last_session_id'] = 0;
-	}
-	$_SESSION['last_session_id']++;
-	$cookie_value = $_SESSION['last_session_id'];
-	setcookie("dvwaSession", $cookie_value);
+	$cookie_value = bin2hex(random_bytes(32));
+	setcookie("dvwaSession", $cookie_value, [ 'httponly' => true, 'samesite' => 'Strict' ]);
 }
+
 ?>
