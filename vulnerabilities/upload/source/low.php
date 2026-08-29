4fc73f3beddfcf00fc0108557075df596a476a14
Jathurshan.R <rdjathurshan@gmail.com>
2026-08-29 01:09:06 -0400

    fix: validate uploaded file is a real image before saving, preventing PHP webshell upload

--- vulnerabilities/upload/source/low.php
+++ vulnerabilities/upload/source/low.php
@@ -1,17 +1,19 @@
 <?php
 
 if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
+	$target_path = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
 	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
 
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
+	$allowed_types = [ 'image/jpeg', 'image/png', 'image/gif' ];
+	$file_info     = getimagesize( $_FILES[ 'uploaded' ][ 'tmp_name' ] );
+
+	if( $file_info === false || !in_array( $file_info[ 'mime' ], $allowed_types, true ) ) {
+		$html .= '<pre>Upload rejected: file is not a valid image.</pre>';
+	}
+	elseif( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
 		$html .= '<pre>Your image was not uploaded.</pre>';
 	}
 	else {
-		// Yes!
 		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
 	}
 }
